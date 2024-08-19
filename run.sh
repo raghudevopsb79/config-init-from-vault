90208b2b2a6b118f69c1ad643fcc2031a27a4c5e
Raghu K <raghuk.vit@gmail.com>
2024-08-19 08:36:42 +0530

    RoboShop Terraform & Kubernetes Code

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -z "$VAULT_TOKEN" ]; then
+  echo VAULT_TOKEN variable is missing
+  exit 1
+fi
+
+if [ -z "$VAULT_ADDR" ]; then
+  echo $VAULT_ADDR variable is missing
+  exit 1
+fi
+
+vault login -tls-skip-verify
+
+
